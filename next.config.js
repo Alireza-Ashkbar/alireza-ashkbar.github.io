4290af44127e2b273bf66c9b057e874ef8f738a3
ar.ashkbar <ar.ashkbar@memseco.com>
2026-01-07 12:33:33 +0330

    d

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // static export
-  experimental: { appDir: true },
-  basePath: '/alireza-ashkbar.github.io', // GitHub Pages repo path
-  images: { unoptimized: true }, // prevents next/image issues on static export
+  
 };
 
 module.exports = nextConfig;
